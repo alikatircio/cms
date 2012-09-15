978c5ad3c6897c7edbe788d4b3ab1c916d231f89
alikatircioglu <ali.katircio@gmail.com>
2012-09-15 16:11:52 +0300

    proje oluşturuldu, gemler kuruldu.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms::Application.config.secret_token = 'b38c4084082d89edb19754adf1aa367b5ca44a282ed617dcdb52c733019721225d090c7f629a4d0366d8288bbd7e90ece80621c93d487e1ce5331e6d0418ae3e'
