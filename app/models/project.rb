a10b8f90192603d3720107559b0a6f5f0afb9f71
alikatircioglu <ali.katircio@gmail.com>
2012-09-16 17:02:59 +0300

    Issue #10 ilişkiler kuruldu.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
   attr_accessible :description, :name, :user_id
   validates :name, :description, :presence => true
   validates :name, :uniqueness => true
+
+  has_many :pages
 end
